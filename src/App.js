3c77aa10b50f06cdf22523ab616cb7e804e1ac6d
Your Name <you@example.com>
2023-07-05 17:45:27 +0545

    added remove button

--- src/App.js
+++ src/App.js
@@ -2,27 +2,51 @@ import React from 'react';
 import  { useState } from 'react';
 //  import List from "./component/List";
 function App () {
-  const [value,setValue] =useState();
+  const [value,setValue] = useState("");
   const [todo,setTodo] = useState([]);
+
   const handleClick = () => {
     if (value !== "" && todo !== "" ) {
     setTodo([...todo,value]);
     setValue("");
   }
     };
-    
+
+    const handleRemove = async (index) => {
+      // await setTodo(todo.splice(index,1));
+      let a = todo.map( (data, i) => {
+        if(i !== index) return data;
+        return null;
+      })
+      // let arr = [...todo];
+      // arr = arr.splice(index, 1);
+      setTodo(a);
+      console.log(a);
+    }
+        
     return(
       <div className='App'>
         <div>
-        <input onChange ={(e) => setValue(e.target.value)}value ={value}/>
+        {/* <input onChange ={(e) => setValue(e.target.value)}  value = {value}/> */}
+        <input onChange ={(e) => setValue(e.target.value)}  value = {value}/>
         <button onClick ={handleClick}>Submit</button>
         </div>
         <div>
           <ol>
-            {todo.map((todos)=> (
-              <li>{todos}</li>
+            {todo && todo.map((todos,index)=> (
+              <div key={index}>
+              <div >{todos}</div>
+              <button type="button" onClick={()=>handleRemove(index)}>
+                Remove
+              </button>
+              </div>
             ))}
           </ol>
+          <div> 
+          </div>
+        </div>
+        <div>
+         
         </div>
       </div>  
     );
---
+++ src/pages/view/delete.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DeleteButton=(props)=> {
+    return (
+      <button onClick= {props.onClick} className={props.className}>
+      <p>{props.title}</p>
+      </button>
+ 
+    )
+  }
+  export default DeleteButton;
